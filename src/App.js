1f3e20dd46a4f466b2e0f6ca560157c737c99f58
prince kalariya <princekalariya179@gmail.com>
2024-03-09 14:22:55 +0530

    # changed

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
           <Routes>
             <Route exact path="/functionbasecomponent_kra/about" element={<About mode={mode} />}></Route>
             <Route
-              path="/functionbasecomponent_kra"
+              path="/functionbasecomponent_kra/home"
               exact
               element={
                 <TextForm
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <Link className="navbar-brand" to="/functionbasecomponent_kra">
           {props.title}
         </Link>
         <button
@@ -25,7 +25,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link className="nav-link" aria-current="page" to="/functionbasecomponent_kra">
+              <Link className="nav-link" to="/functionbasecomponent_kra/home">
                 Home
               </Link>
             </li>
